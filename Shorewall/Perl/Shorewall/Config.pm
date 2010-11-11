6f2aecce19c88bfd83c00816f24d9a70bb756b20
Tom Eastep <teastep@shorewall.net>
2010-11-11 13:00:26 -0800

    Fix for unexpected /usr/share/shorewall/init
    Signed-off-by: Tom Eastep <teastep@shorewall.net>

--- Shorewall/Perl/Shorewall/Config.pm
+++ Shorewall/Perl/Shorewall/Config.pm
@@ -3286,7 +3286,7 @@ sub append_file( $;$$ ) {
     
     $indent = '' if $unindented;
 
-    unless ( $user_exit =~ /^($globals{SHAREDIR})/ ) {
+    unless ( $user_exit =~ /^($globals{SHAREDIRPL})/ ) { #Don't copy from /usr/share/shorewall*
 	if ( -f $user_exit ) {
 	    if ( $nomsg ) {
 		#
